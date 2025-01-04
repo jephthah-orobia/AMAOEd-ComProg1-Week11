941af870faf3d784bc2987584271cd3f903c9168
jephthah-orobia <jephthah.orobia@gmail.com>
2025-01-04 14:55:52 -0600

    implement Dog class

---
+++ src/dog.h
@@ -0,0 +1,44 @@
+#include "legged-mammal.h"
+
+enum class DogSize{
+    ExtraSmall,
+    Small,
+    Medium,
+    ExtraBig
+};
+
+class Dog : LeggedMammal{
+private:
+    std::string breed;
+    bool is_registered = true;
+    DogSize size = DogSize::Medium;
+
+public:
+    Dog(std::string name, std::string breed, DogSize size, bool isRegistered)
+        : LeggedMammal(name, {FurTypes::GuardHairs, FurTypes::Underfur, FurTypes::Whiskers}),
+            breed(breed),
+            size(size),
+            is_registered(isRegistered){};
+    Dog(std::string name, std::string breed, bool isRegistered)
+        : LeggedMammal(name, {FurTypes::GuardHairs, FurTypes::Underfur, FurTypes::Whiskers}),
+            breed(breed),
+            is_registered(isRegistered){};
+    Dog(std::string name, std::string breed, DogSize size)
+        : LeggedMammal(name, {FurTypes::GuardHairs, FurTypes::Underfur, FurTypes::Whiskers}),
+            breed(breed),
+            size(size){};
+    Dog(std::string name, std::string breed)
+        : LeggedMammal(name, {FurTypes::GuardHairs, FurTypes::Underfur, FurTypes::Whiskers}),
+            breed(breed){};
+
+    /* Accessors */
+    std::string getBreed(){return breed;}
+    bool isRegistered(){return is_registered;}
+    DogSize getSize(){return size;}
+
+    /* Setters/Mutators */
+    void setBreed(std::string b){breed = b;}
+    void setIsRegistered(bool r){is_registered = r;}
+    void setSize(DogSize ds){size = ds;}
+
+};
