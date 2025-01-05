4a953949b310862af04537a4686993fd2cb31d1b
jephthah-orobia <jephthah.orobia@gmail.com>
2025-01-04 18:59:14 -0600

    format and add some foldables

--- src/student.h
+++ src/student.h
@@ -1,23 +1,42 @@
 #include "person.h"
 
-class Student : public Person {
+class Student : public Person
+{
 private:
     std::string academic_program;
     unsigned short year;
     std::string university;
 
 public:
-    Student(std::string name, bool isMale, unsigned short age, std::string addr, std::string acadProg, unsigned short year, std::string university)
+    Student(std::string name, bool isMale,
+            unsigned short age, std::string addr,
+            std::string acadProg, unsigned short year,
+            std::string university)
         : Person(name, isMale, age, addr, "Student"),
-            academic_program(acadProg),
-            year(year),
-            university(university) {}
-    
-    std::string getAcademicProgram() const { return academic_program;}
-    unsigned short getYear() const { return year;}
-    std::string getUniversity() const { return university;}
+          academic_program(acadProg),
+          year(year),
+          university(university) {}
 
-    void setAcademicProgram(std::string ap) { academic_program = ap;}
-    void setYear(unsigned short yr) { year = yr;}
-    void setUniversity(std::string univ) { university = univ;}
+    
+    #pragma region Accessors
+    //get Academic Program
+    std::string getAcademicProgram() const { return academic_program; }
+
+    //get College Year
+    unsigned short getYear() const { return year; }
+
+    //get University
+    std::string getUniversity() const { return university; }
+    #pragma endregion Accessors
+
+    #pragma region Mutators
+    //set Academic Program
+    void setAcademicProgram(std::string ap) { academic_program = ap; }
+    
+    //set College Year
+    void setYear(unsigned short yr) { year = yr; }
+
+    //set University
+    void setUniversity(std::string univ) { university = univ; }
+    #pragma endregion Mutators
 };
